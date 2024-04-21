d128efe22ffe3933cdd659db80e8a91f7af4272b
João Campos <joao.victor_55@hotmail.com>
2024-04-21 00:01:23 -0300

    feat(services): remove if and return statement for a cleaner approach

--- src/app/services/cart/cart.service.ts
+++ src/app/services/cart/cart.service.ts
@@ -16,7 +16,7 @@ export class CartService {
 
   public readonly addToCart = (item: CartItemModel): void => {
     const currentItems = this._cartBS.value;
-    const existingItem = currentItems.find(i => i.id === item.id);
+    const existingItem = currentItems.find((i) => i.id === item.id);
 
     if (existingItem) {
       existingItem.quantity += item.quantity;
@@ -28,9 +28,9 @@ export class CartService {
 
   public readonly removeFromCart = (itemId: string): void => {
     const cartItems = this._cartBS.value;
-    const updatedCart = cartItems.filter(cart => cart.id !== itemId);
+    const updatedCart = cartItems.filter((cart) => cart.id !== itemId);
     this._cartBS.next(updatedCart);
-  }
+  };
 
   public readonly addProductQuantity = (itemId: string): void => this.updateProductQuantity(itemId, 'ADD');
 
@@ -38,15 +38,14 @@ export class CartService {
 
   private readonly updateProductQuantity = (itemId: string, operation: 'ADD' | 'REMOVE'): void => {
     const cartItems = this._cartBS.value;
-    const updatedCart = cartItems.map((item) => {
-      if (item.id === itemId) {
-        return {
-          ...item,
-          quantity: operation === 'ADD' ? item.quantity + 1 : item.quantity - 1
-        };
-      }
-      return item;
-    });
+    const updatedCart = cartItems.map((item) =>
+      item.id === itemId
+        ? {
+            ...item,
+            quantity: operation === 'ADD' ? item.quantity + 1 : item.quantity - 1
+          }
+        : item
+    );
 
     this._cartBS.next(updatedCart);
   };
